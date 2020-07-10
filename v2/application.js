962626c2d0d1993849859a276128fa9b6db5ac9c
Graham Hadgraft <grahamhadgraft@grahams-mbp.lan>
2020-07-10 09:52:26 +0100

    Update video API for 2020

--- v2/application.js
+++ v2/application.js
@@ -18,15 +18,14 @@ if (hasGetUserMedia()) {
   window.URL = window.URL || window.webkitURL;
   navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
+const constraints = {
+  audio: false,
+  video: true
+};
+
   if (navigator.getUserMedia) {
-    navigator.getUserMedia({audio: false, video: true}, function(stream) {
-      video.src = window.URL.createObjectURL(stream);
-      video.width = 640;
-      video.height = 480
-      ctx.scale(-1, 1);
-      setInterval(function(){toCanvas();}, 50);
-      localMediaStream = stream;
-    }, onFailSoHard);
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
   } else {
     console.log('fallback fail');
   }
@@ -41,6 +40,18 @@ function toCanvas() {
   ctx.drawImage(video, -640, 0); 
   ctx.drawImage(tache, -480, 200);
 
+}
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+
+  ctx.scale(-1, 1);
+      setInterval(function(){toCanvas();}, 50);
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
 
 function saveImage() {
